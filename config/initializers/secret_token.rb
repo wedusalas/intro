b295c1335024bcdc4cea33c834517270cc4c2c18
loodic.com <vizzlearn@gmail.com>
2013-10-15 18:44:15 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LoodicCom::Application.config.secret_key_base = '7b7afa78e80dde928a0493f0d6634fc444a124ae002eb2a977a6e936e9d83046b1ad97f2cf0153995e4e7c7191469f8cf980251af165b9a8fdb373c1269f93e9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LoodicCom::Application.config.session_store :cookie_store, key: '_loodic_com_session'
